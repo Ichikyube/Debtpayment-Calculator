ab5e6fa902cd98c087c96b1b2a3cc5f895b4310a
Ichikyube <ichikyube@gmail.com>
2023-01-11 13:28:15 +0700

    add img

--- resources/views/livewire/debt-calc.blade.php
+++ resources/views/livewire/debt-calc.blade.php
@@ -6,23 +6,23 @@
     </div>
     <hr class="mt-4 mb-6 border-b-1 border-blueGray-300">
     <div class="flex flex-row align-middle"><div class="flex justify-center w-12 mr-2"><img src="{{asset('img/moneys.svg')}}" alt=""></div>
-    <div class="flex justify-between w-full "><p class="text-base text-gray-400">Jumlah Hutang</p><input class="form-input appearance-none block px-3 border-0 text-right outline-none placeholder:!bg-transparent bg-transparent
-                    transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0" type="text" placeholder="500000"></div>
+    <div class="flex justify-between w-full"><p class="text-base text-gray-400 w-max">Jumlah Hutang</p><input class="form-input appearance-none block px-3 border-0 text-right outline-none placeholder:!bg-transparent bg-transparent
+                    transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0" type="number" min="100" max="" placeholder="500000"></div>
     </div>
     <hr class="mt-4 mb-6 border-b-1 border-blueGray-300">
     <div class="flex flex-row align-middle"><div class="flex justify-center w-12 mr-2"><img src="{{asset('img/moneytime.svg')}}" alt=""></div>
-    <div class="flex justify-between w-full w-max"><p class="text-base text-gray-400 w-max">Suku Bunga Hutang</p><input class="form-input appearance-none block px-3 border-0 text-right outline-none placeholder:!bg-transparent bg-transparent
-                    transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0" type="text" placeholder="15%"></div>
+    <div class="flex justify-between w-full"><p class="text-base text-gray-400 w-max">Suku Bunga Hutang</p><input class="form-input appearance-none block px-3 border-0 text-right outline-none placeholder:!bg-transparent bg-transparent
+                    transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0" type="number" min="0" max="100" placeholder="15%"></div>
     </div>
     <hr class="mt-4 mb-6 border-b-1 border-blueGray-300">
     <div class="flex flex-row align-middle"><div class="flex justify-center w-12 mr-2"><img src="{{asset('img/moneysend.svg')}}" alt=""></div>
     <div class="flex justify-between w-full"><p class="text-base text-gray-400 w-max">Pembayaran minimum perbulan</p><input class="form-input appearance-none block px-3 border-0 text-right outline-none placeholder:!bg-transparent bg-transparent
-                    transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0" type="text" placeholder="500"></div>
+                    transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0" type="number" min="0" max="" placeholder="500"></div>
     </div>
     <hr class="mt-4 mb-6 border-b-1 border-blueGray-300">
     <div class="flex flex-row align-middle"><div class="flex justify-center w-12 mr-2"><img src="{{asset('img/1.svg')}}" alt=""></div>
         <div class="flex justify-between w-full"><p class="text-base text-gray-400 w-max">Pendapatan perbulan</p><input class="form-input appearance-none block px-3 border-0 text-right outline-none placeholder:!bg-transparent bg-transparent
-                    transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0" type="text" placeholder="5000"></div>
+                    transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0" type="number" min="0" max="" placeholder="5000"></div>
     </div>
     <hr class="mt-4 mb-6 border-b-1 border-blueGray-300">
     <div class="flex justify-between text-center">
