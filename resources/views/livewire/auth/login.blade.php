e83185f8016468af2d6dddebc46725e13d657461
tifarin <latifahlatifah443@gmail.com>
2023-01-16 15:21:36 +0700

    responsive register

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -6,29 +6,27 @@
 
             <a href="{{ route('home') }}">
                 <div class="flex justify-center mx-auto mt-20 mb-4"><x-logo/></div>
-                <h2 class="mt-[22px] text-[53px] leading-[70.12px] text-center hidden md:block">
-                    Debt Repayment
-                </h2>
-                <h2 class="mt-[22px] text-[24px] leading-[70.12px] text-center text-white block md:hidden">
+                <h2 class="mt-[22px] text-[24px] md:text-[53px] leading-[70.12px] text-center text-white block">
                     Login
                 </h2>
+                <h2 class="mt-[22px] text-[53px] leading-[70.12px] text-white text-center hidden md:block">
+                    Debt Repayment
+                </h2>
             </a>
-            <p class="text-[36px] leading-[47.63px] text-center hidden md:block">Aplikasi Pelunasan Hutang</p>
-            
         </div>
 
-    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+    <div class="md:mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div class="px-4 py-8 sm:rounded-lg sm:px-10">
             <div>
                 <h1 x-text="error.message"></h1>
                 <div>
-                    <label for="email" class="block text-sm font-medium leading-5 text-slate-500">
+                    <label for="email" class="block text-sm font-medium leading-5 text-white">
                         Email address
                     </label>
 
                     <div class="mt-1 rounded-md">
                         <input x-model="email" type="email" name="email" id="email" required autofocus
-                        class="form-input appearance-none block w-full px-3 py-2 border-0 border-b border-b-slate-400 border-b-solid outline-none placeholder:!bg-transparent bg-transparent
+                        class="form-input text-white appearance-none block w-full px-3 py-2 border-0 border-b border-b-slate-400 border-b-solid outline-none placeholder:!bg-transparent bg-transparent
                         transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0  placeholder="you@example.com" />
                         <br/>
                         <p x-text="validation.email"></p>
@@ -36,12 +34,12 @@
                 </div>
 
                 <div class="mt-6">
-                    <label for="password" class="block text-sm font-medium leading-5 text-slate-500">
+                    <label for="password" class="block text-sm font-medium leading-5 text-white">
                         Password
                     </label>
 
                     <div class="mt-1 rounded-md shadow-sm">
-                        <input x-model="password" type="password" name="password" id="password" required class="appearance-none form-input border-0 border-b border-b-slate-400
+                        <input x-model="password" type="password" name="password" id="password" required class="appearance-none text-white form-input border-0 border-b border-b-slate-400
                         border-b-solid outline-none placeholder:!bg-transparent bg-transparent block w-full px-3 py-2 transition duration-150 ease-in-out sm:text-sm
                         sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0" />
                         <p x-text="validation.password"></p>
@@ -51,13 +49,13 @@
                 <div class="flex items-center justify-between mt-6">
                     <div class="flex items-center">
                         <input wire:model.lazy="remember" id="remember" type="checkbox" class="w-4 h-4 text-indigo-600 transition duration-150 ease-in-out form-checkbox" />
-                        <label for="remember" class="block ml-2 text-sm leading-5 text-gray-900">
+                        <label for="remember" class="block ml-2 text-sm leading-5 text-white">
                             Remember
                         </label>
                     </div>
 
                     <div class="w-32 text-xs leading-5">
-                        <a href="{{ route('password.request') }}" class="font-medium text-indigo-600 transition duration-150 ease-in-out hover:text-indigo-500 focus:outline-none focus:underline">
+                        <a href="{{ route('password.request') }}" class="font-medium text-white transition duration-150 ease-in-out hover:text-indigo-500 focus:outline-none focus:underline">
                             Forgot your password?
                         </a>
                     </div>
@@ -65,15 +63,15 @@
 
                 <div class="mt-6">
                     <span class="block w-full rounded-md shadow-sm bg-main">
-                        <button  x-on:click="submited()" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out border border-transparent rounded-md hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:border-gray-700 focus:ring-indigo active:bg-white">
-                            ENTER
+                        <button  x-on:click="submited()" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-black transition duration-150 ease-in-out border border-transparent rounded-md hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:border-gray-700 focus:ring-indigo active:bg-white">
+                            LOGIN
                         </button>
                     </span>
                 </div>
                 @if (Route::has('register'))
                     <p class="mt-2 text-sm leading-5 text-center text-gray-600 max-w">
                         Or
-                        <a href="{{ route('register') }}" class="font-medium text-indigo-600 transition duration-150 ease-in-out hover:text-indigo-500 focus:outline-none focus:underline">
+                        <a href="{{ route('register') }}" class="font-medium text-white transition duration-150 ease-in-out hover:text-indigo-500 focus:outline-none focus:underline">
                             create a new account
                         </a>
                     </p>
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -6,83 +6,87 @@
     <div x-data="$store.register" class="bg-transparent" >
         <div class="tracking-[4.03px] sm:mx-auto text-dark w-full">
             <a href="{{ route('home') }}">
-                <h2 class="mt-11 mb-4 text-[53px] leading-[70.12px] text-center">
+                <div class="flex justify-center mx-auto mt-20 mb-4"><x-logo/></div>
+                <h2 class="mt-11 mb-4 text-[24px] md:text-[53px] leading-[70.12px] text-center text-white">
+                    Register
+                </h2>
+                <h2 class="mt-11 mb-4 text-[53px] leading-[70.12px] text-center text-white hidden md:block">
                     Debt Repayment
                 </h2>
             </a>
-            <p class="text-[36px] leading-[47.63px] text-center">Aplikasi Pelunasan Hutang</p>
 
 
-            <h2 class="mt-6 text-base font-normal leading-9 text-center text-gray-900">
-                Create a new account
-            </h2>
-
-            <p class="mt-2 text-sm leading-5 text-center text-gray-600 max-w">
-                Or
-                <a href="{{ route('login') }}" class="font-medium transition duration-150 ease-in-out text-main hover:text-indigo-500 focus:outline-none focus:underline">
-                    sign in to your account
-                </a>
-            </p>
+            
         </div>
 
         <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div class="px-4 py-8">
+            <div class="px-4 md:py-8">
                 <p x-text="messages"></p>
                 <div>
-                    <label for="name" class="block text-sm font-medium leading-5 text-gray-700">
+                    <label for="name" class="block text-sm font-medium leading-5 text-white">
                         Name
                     </label>
 
                     <div class="mt-1 rounded-md shadow-sm">
                         <input x-model="name"  name="name" id="name" type="text" required autofocus :class="validation.name !== undefined ? 'text-red-900':''" class="form-input appearance-none block w-full px-3 py-2 border-0 border-b border-b-slate-400 border-b-solid outline-none placeholder:!bg-transparent bg-transparent
-                        transition duration-150 ease-in-out sm:text-sm sm:leading-5focus:border-none  sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0"/>
+                        transition duration-150 ease-in-out sm:text-sm sm:leading-5focus:border-none  sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0 text-white"/>
                     </div>
                     <p class="text-xs" x-text="validation.name"></p>
                 </div>
 
                 <div class="mt-6">
-                    <label for="email" class="block text-sm font-medium leading-5 text-gray-700">
+                    <label for="email" class="block text-sm font-medium leading-5 text-white">
                         Email address
                     </label>
 
                     <div class="mt-1 rounded-md shadow-sm">
-                        <input x-model="email"  name="email" id="email" required :class="validation.email !== undefined ? 'text-red-900':''" class="form-input appearance-none block w-full px-3 py-2 border-0 border-b border-b-slate-400 border-b-solid outline-none placeholder:!bg-transparent bg-transparent
+                        <input x-model="email"  name="email" id="email" required :class="validation.email !== undefined ? 'text-red-900':''" class="form-input appearance-none text-white block w-full px-3 py-2 border-0 border-b border-b-slate-400 border-b-solid outline-none placeholder:!bg-transparent bg-transparent
                         transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0"/>
                     </div>
                     <p class="text-xs" x-text="validation.email"></p>
                 </div>
 
                 <div class="mt-6">
-                    <label for="password" class="block text-sm font-medium leading-5 text-gray-700">
+                    <label for="password" class="block text-sm font-medium leading-5 text-white">
                         Password
                     </label>
 
                     <div class="mt-1 rounded-md shadow-sm">
                         <input x-model="password" type="password" name="password" id="password" required :class="validation.password !== undefined ? 'text-red-900':''" class="form-input appearance-none block w-full px-3 py-2 border-0 border-b border-b-slate-400 border-b-solid outline-none placeholder:!bg-transparent bg-transparent
-                        transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0 @error('password') border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:ring-red @enderror" />
+                        transition duration-150 ease-in-out sm:text-sm sm:leading-5 text-white focus:border-none focus:outline-none focus-visible:ring-0 @error('password') border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:ring-red @enderror" />
                     </div>
                     <p class="text-xs" x-text="validation.password"></p>
                 </div>
 
                 <div class="mt-6">
-                    <label for="password_confirmation" class="block text-sm font-medium leading-5 text-gray-700">
+                    <label for="password_confirmation" class="block text-sm font-medium leading-5 text-white">
                         Confirm Password
                     </label>
 
                     <div class="mt-1 rounded-md shadow-sm">
                         <input x-model="passwordConfirmation" id="password_confirmation" type="password" required class="form-input appearance-none block w-full px-3 py-2 border-0 border-b border-b-slate-400 border-b-solid outline-none placeholder:!bg-transparent bg-transparent
-                        transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none focus:outline-none focus-visible:ring-0" />
+                        transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-none text-white focus:outline-none focus-visible:ring-0" />
                     </div>
                     <p class="text-xs" x-text="validation.password_confirmation"></p>
                 </div>
 
                 <div class="mt-6">
                     <span class="block w-full rounded-md shadow-sm">
-                        <button x-on:click="submited()" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out border border-transparent rounded-md bg-main hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:border-main focus:ring-indigo active:bg-white">
+                        <button x-on:click="submited()" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-black transition duration-150 ease-in-out border border-transparent rounded-md bg-main hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:border-main focus:ring-indigo active:bg-white">
                             Register
                         </button>
                     </span>
                 </div>
+                <h2 class="mt-6 text-base font-normal leading-9 text-center text-white">
+                    Create a new account
+                </h2>
+    
+                <p class="mt-2 text-sm leading-5 text-center text-gray-600 max-w">
+                    Or
+                    <a href="{{ route('login') }}" class="font-medium transition duration-150 ease-in-out text-white hover:text-indigo-500 focus:outline-none focus:underline">
+                        sign in to your account
+                    </a>
+                </p>
             </div>
         </div>
     </div>
