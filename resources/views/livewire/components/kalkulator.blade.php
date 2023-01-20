bfe599b05355fe34f875576f04c463e7c4b1048c
Ichikyube <ichikyube@gmail.com>
2023-01-20 15:39:27 +0700

    kalkulator view

--- resources/views/livewire/components/kalkulator.blade.php
+++ resources/views/livewire/components/kalkulator.blade.php
@@ -1,13 +1,33 @@
 <div x-data="$store.create">
-    <h1 class="my-4 ml-8 text-3xl font-bold lg:my-0 lg:mb-4">Kalkulator Hutang</h1>
+    <div class="flex items-center justify-between mb-2 align-middle">
+        <h1 class="my-4 ml-8 text-3xl font-bold lg:my-0 lg:mb-4">Kalkulator Hutang</h1>
+        <div x-show="!calculated" class="flex items-center px-3 pb-2 text-center justify-evenly">
+            <button type="button" x-on:click.lazy="posts++" class="text-sm py-4 px-7 ml-4
+            bg-white text-red-500 font-bold rounded-[30px] drop-shadow-lg cursor-pointer select-none
+            active:translate-y-1 active:[box-shadow:0_0px_0_0_#f2f2f2,0_0px_0_0_#b7b7b7]
+            active:border-b-[0px] transition-all duration-150 mr-11
+            [box-shadow:0_1px_0_0_#f2f2f2,0_3px_0_0_#b7b7b7]  after:w-6 after:h-6 after:bg-red-500
+            after:text-white  after:font-cursive after:font-extrabold after:absolute after:-right-3
+            after:-scale-x-100  after:rounded-full after:content-['+'] after:align-middle after:text-center">
+                Tambahkan Hutang
+            </button>
+            <button x-on:click="hitung()" class="self-end px-5 text-white shadow drop-shadow-lg cursor-pointer
+            select-none active:translate-y-1 active:[box-shadow:0_0px_0_0_#f2f2f2,0_0px_0_0_#b7b7b7] active:border-b-[0px] rounded-xl
+            [box-shadow:0_1px_0_0_#f2f2f2,0_3px_0_0_#b7b7b7] bg-myblue h-14 lg:w-44 transition-all duration-150">Calculate</button>
+
+        </div>
+    </div>
+
     <template x-if="calculated">
         @livewire('components.hasil-hitungan')
     </template>
 
-    <div x-show="!calculated" class="flex">
-        <div class="flex w-1/2 mx-auto overflow-x-scroll scroll-auto touch-auto overscroll-x-contain hilanginscroll flex-nowrap" id="scrollhorizontal">
+
+    <div x-show="!calculated" class="flex justify-between">
+        <div class="flex flex-col overflow-y-scroll scroll-auto touch-auto hilanginscroll flex-nowrap" id="scrollhorizontal">
             <!-- Form Kalkulator -->
-            <div class="bg-[#F7D3C2] lg:w-[600px]  max-h-[63vh] lg:max-h-full overflow-y-scroll lg:overflow-y-visible hilanginscroll rounded-xl w-11/12 lg:rounded-[30px] shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out">
+            <div class="bg-[#F7D3C2] lg:w-5/6 mb-11 max-w-[63vh] lg:max-w-full overflow-y-scroll lg:overflow-y-visible hilanginscroll rounded-md w-11/12 lg:rounded-[30px]
+                shadow-sm hover:shadow-xl transition-shadow duration-300 ease-in-out">
                 <div class="flex flex-row px-5 py-5 align-middle border-b-2">
                     <h6 class="ml-5 text-xl font-bold text-blueGray-700">Hutang 1</h6>
                 </div>
@@ -44,7 +64,7 @@
                     </div>
                 </div>
 
-                <div class="flex flex-row items-center px-3 py-2 border-b-2">
+                <div class="flex flex-row items-center px-3 py-2">
                     <div class="flex justify-center w-12 mr-2">
                         <img src="{{asset('img/moneysend.svg')}}" alt="" class="h-5">
                     </div>
@@ -61,7 +81,7 @@
                 <div x-html="html"></div>
             </template>
         </div>
-        <div class="bg-[#F7D3C2] rounded-xl lg:rounded-[30px] shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out">
+        <div class="bg-[#F7D3C2] h-fit rounded-md lg:rounded-[30px] shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out">
             <div class="flex items-center justify-between px-3 py-2 text-center">
                 <div class="flex justify-center w-12 mr-2">
                     <img src="{{asset('img/1.svg')}}" alt="" class="h-5">
@@ -88,23 +108,6 @@
             </div>
         </div>
     </div>
-
-    <div x-show="!calculated" class="z-50 flex items-center justify-between px-3 py-2 text-center bottom-10">
-        <button type="button" x-on:click.lazy="posts++" class="text-sm py-4 px-7 ml-4
-        bg-white text-red-500 font-bold rounded-[30px] drop-shadow-lg cursor-pointer select-none
-        active:translate-y-1 active:[box-shadow:0_0px_0_0_#f2f2f2,0_0px_0_0_#b7b7b7]
-        active:border-b-[0px] transition-all duration-150
-        [box-shadow:0_1px_0_0_#f2f2f2,0_3px_0_0_#b7b7b7]  after:w-6 after:h-6 after:bg-red-500
-        after:text-white  after:font-cursive after:font-extrabold after:absolute after:-right-3
-        after:-scale-x-100  after:rounded-full after:content-['+'] after:align-middle after:text-center">
-            Tambahkan Hutang
-        </button>
-        <button  x-on:click="hitung()" class=" self-end px-5 text-white shadow drop-shadow-lg cursor-pointer
-        select-none active:translate-y-1 active:[box-shadow:0_0px_0_0_#f2f2f2,0_0px_0_0_#b7b7b7] active:border-b-[0px] rounded-xl
-        [box-shadow:0_1px_0_0_#f2f2f2,0_3px_0_0_#b7b7b7] bg-myblue h-14 lg:w-44 transition-all duration-150">Calculate</button>
-
-    </div>
-
     <script>
         const scrollContainer = document.getElementById("scrollhorizontal");
 
