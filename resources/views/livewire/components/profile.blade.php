437438e7fb7117e63e9f158d2cbb13ac29ce3493
vauzi <ismanvauzi998@gmail.com>
2023-01-25 13:07:39 +0700

    (-) memberikan shadwo pada alert error

--- resources/views/livewire/components/profile.blade.php
+++ resources/views/livewire/components/profile.blade.php
@@ -7,7 +7,7 @@
         x-transition:leave="transition -translate-y-10 ease-in duration-300"
         x-transition:leave-start="opacity-100 -translate-y-10"
         x-transition:leave-end="-translate-y-10 opacity-0"
-        class="absolute z-30 w-[700px] right-1/2 left-1/4 -top-5 p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+        class="absolute shadow-2xl z-30 w-[700px] right-1/2 left-1/4 -top-5 p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
         <div class="flex items-center">
             <svg aria-hidden="true" class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
             <span class="sr-only">Info</span>
