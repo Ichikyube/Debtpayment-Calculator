9f5fe0900ce90dc757b9b0312ab5dfbdbb544b77
Ichikyube <ichikyube@gmail.com>
2023-01-26 10:23:18 +0700

    fixing show data layout size

--- resources/views/livewire/components/profile.blade.php
+++ resources/views/livewire/components/profile.blade.php
@@ -43,14 +43,14 @@
             <svg aria-hidden="true" class="flex-shrink-0 inline w-10 h-10 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
             <span class="sr-only">Info</span>
             <div class="flex flex-col">
-                <span class="font-medium text-xl">Success</span>
+                <span class="text-xl font-medium">Success</span>
                 <span x-text="message"></span>
             </div>
         </div>
         <button x-on:click="showSuccessAlert = false">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>              
+            </svg>
         </button>
     </div>
     {{-- end success alert --}}
@@ -82,9 +82,9 @@
                         </div>
                     </div>
                     <div class="mt-0 mb-2 text-sm font-bold leading-normal uppercase text-blueGray-400" x-text="user.email" ></div>
-                    <ul class="w-3/4 h-40 p-2 mx-auto mt-10 overflow-y-scroll text-justify transition-colors duration-1000 ease-in-out md:w-1/2 lg:w-1/2 group hover:border hover:border-sky-500 hilanginscroll overflow-clip text-blueGray-600">
-                        <li class="transition-colors duration-1000 ease-in-out border-transparent group-hover:border-b group-hover:border-sky-500"><p class="break-words whitespace-normal"><b> Jenis Kelamin  </b>    <span> : </span> <span x-text="user.gender"></span> </p></li>
-                        <li class="transition-colors duration-1000 ease-in-out border-transparent group-hover:border-b group-hover:border-sky-500"><p class="break-normal whitespace-normal"><b> Tempat, Tgl Lahir  </b> <span> : </span> <span x-text="user.tempat_lahir + ',    '" ></span> <span x-text="user.tgl_lahir"></span> </p></li>
+                    <ul class="max-w-lg p-2 mx-auto mt-10 overflow-y-scroll text-justify transition-colors duration-1000 ease-in-out max-h-44 md:w-1/2 lg:w-full group hover:border hover:border-sky-500/30 hilanginscroll overflow-clip text-blueGray-600">
+                        <li class="transition-colors duration-1000 ease-in-out border-transparent group-hover:border-b group-hover:border-sky-500/30"><p class="break-words whitespace-normal"><b> Jenis Kelamin  </b>    <span> : </span> <span x-text="user.gender"></span> </p></li>
+                        <li class="transition-colors duration-1000 ease-in-out border-transparent group-hover:border-b group-hover:border-sky-500/30"><p class="break-normal whitespace-normal"><b> Tempat, Tgl Lahir  </b> <span> : </span> <span x-text="user.tempat_lahir + ',    '" ></span> <span x-text="user.tgl_lahir"></span> </p></li>
                         <li><p class="whitespace-normal"><b> alamat </b><span class="align-left">: </span></p><p class="break-words whitespace-normal" x-text="user.alamat"></p></li>
                     </ul>
                 </div>
