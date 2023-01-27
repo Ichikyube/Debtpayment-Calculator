defb16a953794033010e1831175260f951d985c8
khoirony <khoironyarief08@gmail.com>
2023-01-27 13:17:59 +0700

    fix notif min bayar

--- resources/views/livewire/components/kalkulator.blade.php
+++ resources/views/livewire/components/kalkulator.blade.php
@@ -12,7 +12,7 @@
             <div class="flex items-center" >
                 <svg aria-hidden="true" class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
                 <span class="sr-only">Info</span>
-                <h3 class="text-lg font-medium">Request Filed!!</h3>
+                <h3 class="text-lg font-medium">Request Failed!!</h3>
             </div>
             <div class="mt-2 mb-4 text-sm">
                 <ul class="mt-1.5 ml-4 list-disc list-inside" x-show="validation">
@@ -23,7 +23,9 @@
                     <li x-show="validation.monthlyInstallments" x-text="validation.monthlyInstallments"></li>
                 </ul>
                 <ul class="mt-1.5 ml-4 list-disc list-inside" x-show="messages">
-                    <li x-show="messages" x-text="messages"></li>
+                    <template x-for="message in messages">
+                        <li x-show="message" x-text="message"></li>
+                    </template>
                 </ul>
             </div>
             <div class="flex justify-end">
@@ -48,7 +50,7 @@
             <svg aria-hidden="true" class="flex-shrink-0 inline w-10 h-10 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
             <span class="sr-only">Info</span>
             <div class="flex flex-col">
-                <span class="text-xl font-medium">Request Filed!!</span>
+                <span class="text-xl font-medium">Request Failed!!</span>
                 <span>Harap isi data dengan benar</span>
             </div>
         </div>
